9c70f8d386d54d35874d92e3b9e9f44ef337d992
Priscilla-22 <priscillamw123@gmail.com>
2024-03-04 16:38:05 +0300

    object inheritance

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,16 @@
 
 from user import User
 
+
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, added_knowledge):
+        return self.knowledge.append(added_knowledge)
+
+
+student = Student('Ann', 'Maria')
+student.learn("hjakhdkjhasjshj")
+print(student.knowledge)
